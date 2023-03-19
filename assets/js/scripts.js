f71e7bd5950350e0f468834381d06b588b492517
Rafael Santos <rafa_santos33@outlook.com>
2023-03-19 20:27:48 -0300

    refatorando código

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,35 +3,35 @@ const email = document.querySelector('#email');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    let input = document.querySelector('input[type="email"]');
-
-    if(email.value === '' || !emailValido(email.value)) {
-        let errorMessage = document.querySelector('.msgErro');
-        errorMessage.classList.add('errorMessage');
-        errorMessage.textContent = 'Digite um email válido';
-        input.classList.add('error');
-        return;
-    }
-
+    
+    form.style.display = 'none';
     let content = document.querySelector('.content');
     content.classList.add('emailSent');
     content.innerHTML = 
         `E-mail de recuperação<br>enviado para<br>${email.value}<br><p>Enviado</p>
-        <span class="material-icons" id="done-icon">done</span> 
+        <span class="material-icons" id="done-icon">done</span>
+        <div class="access">
+            <a href="#">Acessar conta</a>
+        </div>
         `
     ;
-    form.style.display = 'none';
 });
 
-    function emailValido(email) {
-        const emailRegex = new RegExp(
-            /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-z]{2,}$/
-        );
+function emailValido(email) {
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-z]{2,}$/
+    );
 
-        if(emailRegex.test(email)) {
-            return true;
-        }
-
-        return false;
+    if(emailRegex.test(email)) {
+        return true;
     }
+    return false;
+}
+
+function toggleButton() {
+    if(!emailValido(email.value) || email.value === '') {
+        document.querySelector('#btn').disabled = true;
+        return;
+    }   
+    document.querySelector('#btn').disabled = false;
+}
