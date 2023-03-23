c591aa35a1dbaaf54bee3d2dd534e085ea744503
Rafael Santos <rafa_santos33@outlook.com>
2023-03-23 00:16:57 -0300

    Configurando tela de recuperacao

--- frontend/assets/js/recuperacao.js
+++ frontend/assets/js/recuperacao.js
@@ -1,16 +1,21 @@
-let form = document.querySelector('#form');
-const email = document.querySelector('#email');
+let submitButton = document.querySelector('#button');
+let formContent = document.querySelector('.form-content');
+let backIcon = document.querySelector('.div-back');
+const email = document.querySelector('#form-email');
 
-form.addEventListener('submit', (e) => {
+submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     
-    form.style.display = 'none';
-    let content = document.querySelector('.content');
-    content.classList.add('emailSent');
-    content.innerHTML = 
-    `E-mail de recuperação<br>enviado para<br>${email.value}
-    <div><span class="material-icons" id="done-icon">done</span>
-    <p>Enviado</p></div>
+    formContent.style.display = 'none';
+    backIcon.style.display = 'none';
+    let box = document.querySelector('#box');
+    box.classList.add('emailSent');
+    box.innerHTML = 
+        `E-mail de recuperação<br>enviado para<br>${email.value}
+        <div>
+            <span class="material-icons" id="done-icon">done</span>
+            <p>Enviado</p>
+        </div>
         <div class="access">
             <a href="login.html">Acessar conta</a>
         </div>
@@ -31,8 +36,8 @@ function emailValido(email) {
 
 function toggleButton() {
     if(!emailValido(email.value) || email.value === '') {
-        document.querySelector('#btn').disabled = true;
+        document.querySelector('.btn').disabled = true;
         return;
     }   
-    document.querySelector('#btn').disabled = false;
+    document.querySelector('.btn').disabled = false;
 }
