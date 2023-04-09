40c51ff56b2b0f1e7e1071ad1fb96f40e2a1185f
MatheusHenrique95 <mhssantana95@gmail.com>
2023-04-09 17:10:28 -0300

    Finalizada, só falta linkar

--- ima_pay/src/pages/Balance/index.tsx
+++ ima_pay/src/pages/Balance/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.css';
 import { IoPersonCircle } from 'react-icons/io5'
 import { AiFillQuestionCircle, AiOutlineEye } from 'react-icons/ai'
 import ImaPayLogo from '../../assets/logo-pay-ima.png'
-import { MdPix } from "react-icons/md"
+import { MdPix, MdArrowCircleRight } from "react-icons/md"
 import { RiBarcodeFill } from "react-icons/ri"
 import { TbArrowsTransferDown, TbArrowsTransferUp } from "react-icons/tb"
 
@@ -33,7 +33,7 @@ export function Balance() {
                                 contatos PIX. A solução para a falta de tempo e sem taxas de transferência.
                             </p>
                         </div>         
-                    <div className={styles.cardTransfer}>
+                                <div className={styles.cardTransfer}>
                         <section>
                             <TbArrowsTransferUp size={40} color='#fff'/>
                             <p>Transferências</p>
@@ -56,13 +56,64 @@ export function Balance() {
                             </section>
                             <p>Pague as suas contas sem precisar enfretar fila, diretamente do app/site com nosso leitor de códigos de barras.
                             </p>
-                        </div>             
+                        </div> 
                 </div>
                 <div className={styles.container}>
-
+                    <div className={styles.realBalance}>
+                        <h1>R$ 554,82</h1>
+                        <p>115% CDI <span>R$ 13,54 neste mês</span></p>                     
+                    </div>
+                    <div className={styles.balanceDetails}>
+                        <p>Saldo da conta: R$ 554,82</p>
+                        <p>Disponível Cheque especial: R$ 200,00</p>
+                        <p>Total disponível para saque: R$ 754,82</p>
+                    </div>
+                    <div className={styles.lastTransfer}>
+                        <h3>Últimas transações</h3>
+                        <section>
+                            <p>14:25<br/>
+                               23/03/2023 
+                            </p>
+                            <h2>
+                                R$ -114,98
+                            </h2>
+                            <h4>PIX</h4>
+                            <h4>BB</h4>
+                            <p>João das Neves Neto</p>
+                        </section>   <section>
+                            <p>08:38<br/>
+                               23/03/2023 
+                            </p>
+                            <h2>
+                                R$ 94,98
+                            </h2>
+                            <h4>DEP</h4>
+                            <h4>NU</h4>
+                            <p>Maria das Neves Neto</p>
+                        </section>
+                    </div>
+                    <div className={styles.footerTransfer}>
+                        <h3>Histórico de transação</h3>
+                        <MdArrowCircleRight size={32} color='var(--theme)'/>
+                    </div>
                 </div>
                 <div className={styles.container}>
-
+                    <div className={styles.sideCard}>
+                        <h2>Cartão de crédito</h2>
+                        <section>
+                            <h3>Fatura Atual</h3>
+                            <h1>R$ 2376,17</h1>
+                            <p>Limite disponível <span>R$ 372,78</span></p>
+                        </section>
+                    </div>
+                    <div className={styles.sideCard}>
+                        <h2>Empréstimo</h2>
+                        <section>
+                            <h3>limite disponível</h3>
+                            <h1>R$ 25000,00</h1>
+                            <p>Consulte as condições de empréstimo</p>
+                        </section>
+                    </div>
                 </div>
             </div>
         </>
