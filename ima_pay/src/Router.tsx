1b56b5d20fd89f81a1bafcec198ea8fb00e0a2b7
Rafael Santos <rafa_santos33@outlook.com>
2023-04-12 08:57:26 -0300

    update

--- ima_pay/src/Router.tsx
+++ ima_pay/src/Router.tsx
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import { Home } from './pages/Home';
-import { Routes, Route } from 'react-router-dom';
-import { Register } from './pages/Register';
-import { Login } from './pages/Login';
-import { Recovery } from './pages/Recovery';
-import { UserPage } from './pages/User';
-
-export function Router() {
-    return (
-      <>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/user/:userId' element={<UserPage />} />
-          <Route path='/recovery' element={<Recovery />} />
-        </Routes>
-      </>
-    );
-  }
-=======
 import { Routes, Route } from "react-router-dom";
 import { Transfers } from "./pages/Transfers";
 import { SearchTransfers } from "./pages/SearchTransfers";
@@ -46,4 +24,3 @@ export function Router() {
         </Routes>
     );
 }
->>>>>>> 80470215f25d557f58a33486ab1f7393c720a440
